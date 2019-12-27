c974213ba361bc388015ea1e108e9d5ca5548bcc
darylmcneil <darylmcneil5@gmail.com>
2019-12-27 17:39:39 -0500

    upload script configs

--- DevOps_Microservices/project-ml-microservice-kubernetes/upload_docker.sh
+++ DevOps_Microservices/project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,7 +5,8 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+# dockerpath=<your docker ID/image tag>
+dockerpath=cymba123/daryl-py-app
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push cymba123/daryl-py-app
